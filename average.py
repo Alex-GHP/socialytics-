9aa6ab09e170dffad638fb73414472e12fb4a76d
Alex-GHP <alex.morariu.dev@gmail.com>
2024-05-25 08:10:45 +0300

    average algorithm

---
+++ average.py
@@ -0,0 +1,22 @@
+# Pseudocode:
+
+    # if length of nums list == 0: return None
+
+    # Initialize sum to 0
+
+    # Iterate each num
+
+    # Add each num to sum
+
+    # return average -> sum / length of nums
+
+
+def average(nums):
+    if len(nums) == 0:
+        return None
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum / len(nums)
+
+print(average([12, 12, 12]))
---
+++ sum.py
@@ -0,0 +1,18 @@
+# Pseudocode
+
+# Set sum equals 0 at the start
+
+# Iterate each num
+
+# Add each num to the total sum
+
+# return sum
+
+def sum(nums):
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum
+
+
+print(sum([1, 3, 2, 69, 3, 5, 1]))

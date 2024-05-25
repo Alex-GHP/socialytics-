6ffbaf9c6a091499d2c63c1c368d4ac3aa5061bb
Alex-GHP <alex.morariu.dev@gmail.com>
2024-05-25 08:00:33 +0300

    find minimum algorithm with pseudocode

--- find_minimum.py
+++ find_minimum.py
@@ -1,3 +1,13 @@
+# Pseudocode
+
+# Set minimum to infinity
+
+# Iterate each num
+
+# If current num is less than minimum, minimum takes it's value
+
+# return minimum
+
 def find_minimum(nums):
     minimum = float("inf")
     for num in nums:

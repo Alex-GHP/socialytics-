5514e2e56e8dae48f7d0da9dbe1ac00e0455a549
Alex-GHP <alex.morariu.dev@gmail.com>
2024-05-26 08:07:35 +0300

    logarithms: get influencer score

---
+++ get_influencer_score.py
@@ -0,0 +1,9 @@
+# get_influencer_score formula: average_engagement_percentage * log2(num_followers)
+
+import math
+
+
+def get_influencer_score(num_followers, average_engagement_percentage):
+    return round(average_engagement_percentage * math.log(num_followers, 2))
+
+print(get_influencer_score(750000, 0.7))

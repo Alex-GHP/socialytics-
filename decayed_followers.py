84cf67396f8e93f2ac5df3daf798c40b5aba90c6
Alex-GHP <alex.morariu.dev@gmail.com>
2024-05-26 08:45:24 +0300

    exponents: decayed followers

---
+++ decayed_followers.py
@@ -0,0 +1,13 @@
+# Complete the decayed_followers function.
+
+# It calculates the final value of a quantity after a certain time has passed, given its initial value and a rate of decay. Return the remaining followers.
+
+# remaining_total = quantity * ( retention_rate ^ time )
+
+# The retention_rate is the opposite of fraction_lost_daily. If an influencer lost 0.2 (or 20%) of their followers each day, then the retention rate would be 0.8 (or 20%).
+
+def decayed_followers(intl_followers, fraction_lost_daily, days):
+    retention_rate = 1 - fraction_lost_daily
+    return intl_followers * (retention_rate ** days)
+
+print(decayed_followers(200, 0.5, 3))

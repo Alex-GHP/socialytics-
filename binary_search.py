ee71c6c5e8f3ab8af9d6cccafe7af81eeab6b72b
Alex-GHP <alex.morariu.dev@gmail.com>
2024-05-27 08:10:11 +0300

    O(logn): binary search

---
+++ binary_search.py
@@ -0,0 +1,34 @@
+# Pseudocode:
+
+    # Set low = 0
+
+    # Set high = len(arr) - 1
+
+    # while low <= high
+
+        # set median to (low + high) // 2
+
+        # if arr[median] == target: return True
+
+        # elif arr[median] < target: low = median + 1
+
+        # else: high = median - 1
+
+    # return False
+
+def binary_search(target, arr):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        median = (low + high) // 2
+        if arr[median] == target:
+            return True
+        elif arr[median] < target:
+            low = median + 1
+        else:
+            high = median - 1
+    return False
+
+# O(logn)
+
+print(binary_search(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
